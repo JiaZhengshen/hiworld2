88b9703775b04ac86975e69b4420b8531fb0c2d8
HiWorld <https://github.com/JiaZhengshen/hiworld1.git>
2020-12-05 22:59:01 +0800

    test

---
+++ src/test/java/com/atjzs/hiworld/HiworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atjzs.hiworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
